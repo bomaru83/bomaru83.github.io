aa031e2693e21726fb3c94702b8f450ebb4931df
minkyung <bomaru83@naver.com>
2022-05-21 21:08:35 +0900

    add todo remove function

--- src/todo.js
+++ src/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
+    const button = document.createElement("button");
     span.innerText = newTodo;
+    button.innerText= "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
+    console.log(newTodo, toDoList);
 }
 
 function handleToDoSubmit(event) {
@@ -16,3 +26,5 @@ function handleToDoSubmit(event) {
     toDoInput.value = "";
     paintToDo(newTodo);
 }
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
