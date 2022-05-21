edf560880adfe3516eedfd94e77c80633d693290
minkyung <bomaru83@naver.com>
2022-05-21 19:51:24 +0900

    add clock

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = `${hours}시 ${minutes}분 ${seconds}초`;
   }
   
 getClock();
